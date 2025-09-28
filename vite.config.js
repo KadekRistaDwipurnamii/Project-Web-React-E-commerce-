52b1aff5dd8f3e2860e4d13cbe8a3265df97a39f
KadekRistaDwipurnamii <164159383+KadekRistaDwipurnamii@users.noreply.github.com>
2025-09-28 12:53:34 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Project-Web-React-E-commerce-/',
+  base: '/Project-Web-React-E-commerce-/', // sesuai nama repo
 })
